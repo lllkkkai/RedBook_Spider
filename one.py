7475a0f527069ba5ad3e248d98c1f09b6368a6c1
LLLKKK <lllkkk.ai@qq.com>
2024-04-01 00:59:44 +0800

    搜索结果爬取修改为单一文件存储，并使用jieba分词计算词频

--- one.py
+++ one.py
@@ -1,4 +1,6 @@
 import json
+import time
+
 import requests
 from xhs_utils.xhs_util import get_headers, get_params, js, check_cookies, get_note_data, handle_note_info, norm_str, check_and_create_path, save_note_detail, download_media
 
@@ -39,28 +41,31 @@ class OneNote:
             title = norm_str(note.title)
             if title.strip() == '':
                 title = f'无标题'
-            path = f'./{dir_path}/{nickname}_{user_id}/{title}_{note.note_id}'
-            exist = check_and_create_path(path)
-            if exist and not need_cover:
-                print(f'用户: {nickname}, 标题: {title} 本地已存在，跳过保存')
-                return note
-            save_note_detail(path, note)
-            note_type = note.note_type
-            if note_type == 'normal':
-                for img_index, img in enumerate(note.image_list):
-                    img_url = img['info_list'][1]['url']
-                    download_media(path, f'image_{img_index}', img_url, 'image', f'第{img_index}张图片')
-
-            elif note_type == 'video':
-                img_url = note.image_list[0]['info_list'][1]['url']
-                download_media(path, 'cover', img_url, 'image', '视频封面')
-                video_url = note.video_addr
-                download_media(path, 'video', video_url, 'video')
+            save_note_detail('./datas_search', note)
+            # path = f'./{dir_path}/{nickname}_{user_id}/{title}_{note.note_id}'
+            # exist = check_and_create_path(path)
+            # if exist and not need_cover:
+            #     print(f'用户: {nickname}, 标题: {title} 本地已存在，跳过保存')
+            #     return note
+            # save_note_detail(path, note)
+            # note_type = note.note_type
+            # if note_type == 'normal':
+            #     for img_index, img in enumerate(note.image_list):
+            #         img_url = img['info_list'][1]['url']
+            #         download_media(path, f'image_{img_index}', img_url, 'image', f'第{img_index}张图片')
+            #
+            # elif note_type == 'video':
+            #     img_url = note.image_list[0]['info_list'][1]['url']
+            #     download_media(path, 'cover', img_url, 'image', '视频封面')
+            #     video_url = note.video_addr
+            #     download_media(path, 'video', video_url, 'video')
             print(f'用户: {nickname}, {info}标题: {title} 笔记保存成功')
             print('===================================================================')
+            time.sleep(0.2)
             return note
-        except:
-            print(f'笔记 {url} 保存失败')
+        except Exception as e:
+            print(f'笔记 {url} 保存失败:')
+            print(e)
             return
 
     def main(self, url_list):
@@ -74,7 +79,8 @@ class OneNote:
 if __name__ == '__main__':
     one_note = OneNote()
     url_list = [
-        'https://www.xiaohongshu.com/explore/64356527000000001303282b',
+        'https://www.xiaohongshu.com/explore/65f17f17000000000d00cd02',
+        # 'https://www.xiaohongshu.com/explore/64356527000000001303282b',
         # 'https://www.xiaohongshu.com/explore/63d625f8000000001d01042c',
         # 'https://www.xiaohongshu.com/explore/61ac8820000000002103a8aa',
         # 'https://www.xiaohongshu.com/explore/62d2699c000000001200f101',
